02759a9f3c27b982da9b55bc7f157f6ffdc2bde4
Ceci Herriman <156848251+ceci-herriman@users.noreply.github.com>
2026-03-22 19:02:14 -0400

    Update Queries.scala

--- Queries.scala
+++ Queries.scala
@@ -24,6 +24,49 @@ import org.apache.spark.{SparkConf, SparkContext}
   //this is run in my shared_folder/project3/ directory, where my scala file + spark sbt file structure is
   //so when I make new data with data.py or recompile my .scala, I don't have to copy files over
 
+object Query3 {
+  def isClose(x1: Double, y1: Double, x2: Double, y2: Double): Boolean = {
+    val dx = x1 - x2
+    val dy = y1 - y2
+    (dx * dx + dy * dy) <= 36.0
+  }
+
+  def main(args: Array[String]): Unit = {
+    val sparConf = new SparkConf().setMaster("local").setAppName("Query1")
+    val sc = new SparkContext(sparConf)
+    val p = sc.textFile("file:////home/ds503/shared_folder/project3/People_with_handshake_info.txt")
+
+    //map --> (id, (x, y)) if connected is yes --> connected and cache it since its assumed to be small in project desc
+    val connected = p
+      .map(_.split(","))
+      .filter(parts => parts(6) == "yes")
+      .map(parts => (parts(0), (parts(1).toDouble, parts(2).toDouble)))
+
+
+    val connectedBroadcast = sc.broadcast(connected.collect())
+
+    //for each person, map (id, 1) for each connected person with id "id" they are close to
+    //case (id1, (x1, y1, a1, b1, c1))
+    val res = p
+    .flatMap { line =>
+      val parts = line.split(",")
+      connectedBroadcast.value.collect {
+        //get the connected ids the person is close to and map them out
+        case (id2, (x2, y2)) 
+        if parts(0) != id2 && isClose(parts(1).toDouble, parts(2).toDouble, x2, y2) => id2
+      }
+    }
+    .map(id2 => (id2, 1))
+    .reduceByKey(_ + _) //add up all values
+    .collect()
+
+    res.foreach(println)
+
+    sc.stop()
+  }
+}
+
+//I need to optimized these following queries more later - ceci
 
 object Query2 {
   def isClose(x1: Double, y1: Double, x2: Double, y2: Double): Boolean = {
@@ -59,9 +102,18 @@ object Query2 {
     //return pi, connect-i for those which are close to each toher
     val res = people.flatMap { case (id2, (x2, y2, a2, b2, c2)) =>
       connectedBroadcast.value.collect {
-        case (id1, (x1, y1, a1, b1, c2)) if id1 != id2 && isClose(x1, y1, x2, y2) => (id2)
+        case (id1, (x1, y1, a1, b1, c1)) if id1 != id2 && isClose(x1, y1, x2, y2) => (id2)
       }
     }.distinct()
+
+     .flatMap { line =>
+      val parts = line.split(",")
+      connectedBroadcast.value.collect {
+        //get the connected ids the person is close to and map them out
+        case (id2, (x2, y2)) 
+        if parts(0) != id2 && isClose(parts(1).toDouble, parts(2).toDouble, x2, y2) => id2
+      }
+    }
 
     res.foreach(println)
 
@@ -105,8 +157,8 @@ object Query1 {
     //return pi, connect-i for those which are close to each toher
     val res = people.flatMap { case (id2, (x2, y2, a2, b2, c2)) =>
       connectedBroadcast.value.collect {
-        case (id1, (x1, y1, a1, b1, c2)) if id1 != id2 && isClose(x1, y1, x2, y2) =>
-          ((id2, x2, y2, a2, b2, c2), (id1, x1, y1, a1, b1, c2))
+        case (id1, (x1, y1, a1, b1, c1)) if id1 != id2 && isClose(x1, y1, x2, y2) =>
+          ((id2, x2, y2, a2, b2, c2), (id1, x1, y1, a1, b1, c1))
       }
     }
 
